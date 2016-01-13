a9c473c17ed9b0d9ebb0a824f6798edb002fdae5
Jozef Izso <jozef.izso@gmail.com>
2016-01-13 16:13:46 +0100

    Prototype for build finished listener that will process the FogBugz Notifier build feature and call FogBugz API.

--- teamcity-fogbugz-notifier-server/src/main/java/teamcity/FogBugzNotifier.java
+++ teamcity-fogbugz-notifier-server/src/main/java/teamcity/FogBugzNotifier.java
@@ -1,13 +1,18 @@
 package teamcity;
 
+import com.intellij.openapi.diagnostic.Logger;
 import jetbrains.buildServer.serverSide.BuildServerAdapter;
+import jetbrains.buildServer.serverSide.SBuildFeatureDescriptor;
 import jetbrains.buildServer.serverSide.SBuildServer;
 import jetbrains.buildServer.serverSide.SRunningBuild;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.PostConstruct;
+import java.util.Map;
 
 public class FogBugzNotifier extends BuildServerAdapter {
+    private static Logger LOG = Logger.getInstance(FogBugzNotifier.class.getName());
+
     private SBuildServer server;
 
     public FogBugzNotifier(@NotNull SBuildServer server) {
@@ -21,6 +26,11 @@ public class FogBugzNotifier extends BuildServerAdapter {
 
     @Override
     public void buildFinished(@NotNull SRunningBuild build) {
-        super.buildFinished(build);
+        for (SBuildFeatureDescriptor feature : build.getBuildFeaturesOfType(FogBugzNotifierBuildFeature.FEATURE_TYPE)) {
+            Map<String, String> params = feature.getParameters();
+            String url = params.get("fbAddress");
+
+            LOG.debug(String.format("Build feature '%s': server address=%s", feature.getType(), url));
+        }
     }
 }
